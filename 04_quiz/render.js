cb6bdd302e13697ca21961dd977b064e7a730b56
attila5287 <atiturkoz@hotmail.com>
2021-01-20 21:58:51 -0700

    render data default then view by interact /react

--- 04_quiz/render.js
+++ 04_quiz/render.js
@@ -1,29 +1,38 @@
-index = 4;
-const data = questions;
-const length = data.length;
-const step = Math.floor( 100 / length );
-$progress = document.querySelector( "#progress" );
-$progress.setAttribute( "step", step );
-const perc = step * index;
-const pro_style = "width: " + perc + "%; ";
-$progress.setAttribute("style", pro_style);
-$progress.setAttribute("aria-valuenow", perc);
-const d = data[ index ];
+render( questions );
 
-console.log( 'question :>> ', d );
-
-$div = document.querySelector( ".question" );
-const $title = $div.querySelector( ".title" );
-$title.textContent = d.title;
-const choices = d.choices;
-$choices = $div.querySelectorAll(".choice_text")
-
-for (let i = 0; i < choices.length; i++) {
-  const c = choices[ i ];
-  const $c = $choices[ i ];
-  $c.textContent = c;
+function render ( data ) {
+  $range =  document.querySelector("#range");
+  let index =$range.value;
+  console.log('index :>> ', index);
+  $range.addEventListener( "change", ( event ) => {
+    console.log("event.target.id :>> ", $range.value);
+    
+  });
+  const length = data.length;
+  const step = Math.floor( 100 / length );
+  $progress = document.querySelector( "#progress" );
+  $progress.setAttribute( "step", step );
+  const perc = step * index;
+  const pro_style = "width: " + perc + "%; ";
+  $progress.setAttribute( "style", pro_style );
+  $progress.setAttribute( "aria-valuenow", perc );
   
-  
+  const d = data[ index ];
+
+  console.log( 'question :>> ', d );
+
+  $div = document.querySelector( ".question" );
+  const $title = $div.querySelector( ".title" );
+  $title.textContent = d.title;
+  const choices = d.choices;
+  $choices = $div.querySelectorAll( ".choice_text" );
+
+  for ( let i = 0; i < choices.length; i++ ) {
+    const c = choices[ i ];
+    const $c = $choices[ i ];
+    $c.textContent = c;
+
+  }
 }
 // console.log('choices :>> ', choices);
 
