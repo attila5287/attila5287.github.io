187072b41d1dd3fc79319d041e273a05dbed4ce0
attila5287 <atiturkoz@hotmail.com>
2021-01-30 21:20:30 -0700

    test with interval of current

--- 05_day_planner/app.js
+++ 05_day_planner/app.js
@@ -1,19 +1,9 @@
 $(document).ready(function () {
   
-  function init() {
-  	const working_hours = (start_hr, finish_hr) => {
-			let res = [];
-			for (let index = start_hr; index <= finish_hr; index++) {
-				const hour = index;
-				res.push();
-			}
-			return res;
-		};
-
-		let start = 9;
-		
-		let finish = 18;
-		for (let index = start; index < finish; index++) {
+  function init(start, finish) {
+		for ( let index = start; index < finish; index++ ) {
+			$( "#slider" ).attr( "min", start );
+			$( "#slider" ).attr("max", finish );
 			let row = $("<div>");
 			$("#main").append(row);
 			row.attr("class", "row slot mt-2");
@@ -24,7 +14,7 @@ $(document).ready(function () {
 
 			let icon_pre = $("<h4>");
       icon_pre
-				.attr("class", "far fa-clock mt-3 icon")
+				.attr("class", "far fa-clock mt-3 icon_pre")
 				.attr("data-index", index);
 			prepend_div.append(icon_pre);
 			icon_pre.text(" " + index);
@@ -49,14 +39,15 @@ $(document).ready(function () {
 
 			let icon_append = $("<h2>");
 			icon_append
-				.attr("class", "far fa-save text-success icon")
+				.attr("class", "fas fa-hourglass-start text-success icon_app")
 				.attr("data-index", index);
 			append_div.append(btn_save);
 			btn_save.append(icon_append);
       
 		}
-  }
-  init();
+	}
+	let starting_hour= 9; 
+  init(starting_hour, 5+12);
 
   function update_forms ( hour ) {
     // console.log('hour :>> ', hour);
@@ -69,7 +60,10 @@ $(document).ready(function () {
          if ( scheduled == hour ) {
 
            $(this).addClass("border-warning");
-          }
+          } else if (scheduled < hour) {
+						
+						$(this).removeClass("border-warning");
+					}
         }
 			);
     
@@ -89,16 +83,43 @@ $(document).ready(function () {
 			}
 		});
   }
-  function update_icons(hour) {
-		$.each($(".icon"), function (i, el) {
+  function update_icons_app(hour) {
+		$.each($(".icon_app"), function (i, el) {
 			const scheduled = +$(this).attr("data-index");
       if ( scheduled == hour ) {
-        console.log( 'scheduled :>> ', scheduled );
+				console.log( 'scheduled :>> ', scheduled );
+				$(this).removeClass("fas fa-hourglass-start");
+				$(this).addClass("fas fa-hourglass-half");
 				$(this).removeClass("text-success");
 				$(this).removeClass("text-secondary");
         $(this).addClass("text-warning");
         
 			} else if (scheduled < hour) {
+				$(this).removeClass("fas fa-hourglass-start");
+				$(this).removeClass("fas fa-hourglass-half");
+				$(this).addClass("fas fa-hourglass-end");
+				$(this).removeClass("text-success");
+				$(this).removeClass("text-warning");
+        $(this).addClass("text-light");
+			}
+		});
+  }
+	function update_icons_pre ( hour ) {
+		
+		$.each($(".icon_pre"), function (i, el) {
+			const scheduled = +$(this).attr("data-index");
+      if ( scheduled == hour ) {
+				console.log( 'scheduled :>> ', scheduled );
+				$(this).removeClass("far fa-clock");
+				$(this).addClass("fas fa-clock text-xl");
+				$(this).removeClass("text-success");
+				$(this).removeClass("text-secondary");
+        $(this).addClass("text-warning");
+        
+			} else if (scheduled < hour) {
+				$(this).removeClass("far fa-clock");
+				$(this).removeClass("fas fa-clock");
+				$(this).addClass("fas fa-history");
 				$(this).removeClass("text-success");
 				$(this).removeClass("text-warning");
         $(this).addClass("text-light");
@@ -107,8 +128,19 @@ $(document).ready(function () {
   }
   // let current_hour = moment().hour();
   // update_forms(current_hour);
-  let current = 15;
-  update_icons(current);
-  update_forms(current);
-  update_btns(current);
+	
+	test_interval( starting_hour, update_icons_pre, update_icons_app, update_forms, update_btns );
+	
 });
+function test_interval ( starting_hour, update_icons_pre, update_icons_app, update_forms, update_btns ) {
+	let current = starting_hour;
+	setInterval( () => {
+		current++;
+		update_icons_pre( current );
+		update_icons_app( current );
+		update_forms( current );
+		update_btns( current );
+		$( "#slider" ).val( current );
+	}, 750 );
+}
+
