1a4e6e312e01f24e55b8efdcec1825adc13286ec
attila5287 <atiturkoz@hotmail.com>
2021-02-03 09:04:47 -0700

    thme

--- 06_weather_api/Master/forecast.js
+++ 06_weather_api/Master/forecast.js
@@ -1,1 +1,20 @@
-console.log('test forecast');
+// console.log( 'test forecast' );
+const city = 'Denver';
+
+const API_key = "42a5a7b661c854194cb0539e5fd1a86f";
+
+const forecast_url =
+	"https://api.openweathermap.org/data/2.5/forecast?q=" +
+	city +
+	"&appid=" +
+	API_key;
+
+// console.log('forecast_url :>> ', forecast_url);
+$.ajax({
+  type: "GET",
+  url: forecast_url,
+} ).then( ( w ) => {
+  console.log( 'w :>> ', w );
+  console.table(w);
+  console.log( 'test ajax forecast');
+});
