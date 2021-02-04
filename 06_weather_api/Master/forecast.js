fe99fbcbfd1739c56cbfd1c92e537e15b886f84d
attila5287 <atiturkoz@hotmail.com>
2021-02-03 20:53:25 -0700

    denver today

--- 06_weather_api/Master/forecast.js
+++ 06_weather_api/Master/forecast.js
@@ -3,18 +3,16 @@ const city = 'Denver';
 
 const API_key = "42a5a7b661c854194cb0539e5fd1a86f";
 
-const forecast_url =
-	"https://api.openweathermap.org/data/2.5/forecast?q=" +
-	city +
-	"&appid=" +
-	API_key;
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}`;
 
-// console.log('forecast_url :>> ', forecast_url);
+// console.log('url :>> ', url);
 $.ajax({
   type: "GET",
-  url: forecast_url,
+  url: url,
 } ).then( ( w ) => {
-  console.log( 'w :>> ', w );
-  console.table(w);
-  console.log( 'test ajax forecast');
+  // console.log( 'test ajax forecast');
+  console.log( 'w :>> ', w.city );
+  console.log( "w :>> ", convert2HHMM( w.city.sunrise ) );
+  console.log( 'w :>> ', convert2HHMM( w.city.sunset ) );
+  console.log( 'w :>> ', w.list[ 0 ].weather[ 0 ] );
 });
