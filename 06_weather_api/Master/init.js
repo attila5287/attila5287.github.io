4736211808c6a526cc30c01693b292883e8ca035
attila5287 <atiturkoz@hotmail.com>
2021-02-09 16:24:27 -0700

    slider works with set interval of 500ms

--- 06_weather_api/Master/init.js
+++ 06_weather_api/Master/init.js
@@ -1,13 +1,23 @@
 console.log('init test');
 
-
 const default_city = 'Denver';
 forecast_five_days(default_city);
-todays_weather(default_city);
+todays_weather( default_city );
+let index = 1;
+setInterval( ( ) => {
+	update_slider( index, default_city ); 
+	if (index == 40) {
+		update_slider( 1, default_city ); 
+		
+	} else {
+		index++
+		
+	}
+	
+}, 500);
 
-$("#search-value").on("change", function () {
+$("#search-value").on("change",  () => {
 	const city_input = $(this).val();
 	forecast_five_days(city_input);
 	todays_weather( city_input );
-	update_slider(8, city_input); 
 } );
