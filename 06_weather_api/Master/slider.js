e3039f9f307364c604abbfae406704cead11f407
attila5287 <atiturkoz@hotmail.com>
2021-02-08 20:17:45 -0700

    slider works needs to be func'd

--- 06_weather_api/Master/slider.js
+++ 06_weather_api/Master/slider.js
@@ -1,16 +1,45 @@
 
-function update_slider (v) {
-  $( '#slider' ).val( v );
-  $( "#slider_index" ).text( v );
-  const s = $('#slider');
+function update_slider(v, city) {
+	const API_key = "42a5a7b661c854194cb0539e5fd1a86f";
+	const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}`;
+	// console.log('url :>> ', url);
+
+	$.ajax({
+		type: "GET",
+		url: url
+	}).then((w) => {
+		console.log("w :>> ", w.list[v]);
+		console.log("w :>> ", w.list[v].weather[0]);
+		console.log("w :>> ", w.list[v].weather[0].main);
+    console.log( "w :>> ", w.list[ v ].weather[ 0 ].description );
+    
+    $("#slider_description").text(w.list[v].weather[0].description);
+    $("#slider_icon").attr(
+			"src",
+			"http://openweathermap.org/img/wn/" +
+				w.list[v].weather[0].icon +
+				"@2x.png"
+		);
+    const s = $("#slider");
+    s.on("change", function () {
+      const changed = $("#slider").val();
+      $("#slider").val(changed);
+      $("#slider_index").text(`${city}: ${changed * 3} hours later`);
+      
+      $("#slider_description").text(w.list[changed].weather[0].description);
+      $("#slider_icon").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          w.list[changed].weather[0].icon +
+          "@2x.png"
+      );
+    });
+    // $("#slider_icon").attr("src", w[v]);
+	});
   
-  s.on( "change", function () {
-    const changed = $("#slider").val();
-    
-    $( '#slider' ).val( changed );
-    $("#slider_index").text(changed);
-    
-  });
+  $( "#slider" ).val( v );
+  $( "#slider_index" ).text( `${city}: ${v * 3} hours later` );
+  
 }
 
-update_slider(1); 
+update_slider(8, "Denver"); 
