dcc2161bbea416d44e1240e8d39db6fdb82e6c9c
attila5287 <atiturkoz@hotmail.com>
2021-02-10 04:29:12 -0700

    aaa

--- 06_weather_api/Master/init.js
+++ 06_weather_api/Master/init.js
@@ -3,7 +3,8 @@ console.log('init test');
 
 const default_city = 'Denver';
 forecast_five_days(default_city);
-todays_weather(default_city);
+todays_weather( default_city );
+
 
 $("#search-value").on("change", function () {
 	const city_input = $(this).val();
