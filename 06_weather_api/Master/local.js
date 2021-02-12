2d0c328f94931b7b37819b4c5f731dd5f0b43fd9
attila5287 <atiturkoz@hotmail.com>
2021-02-12 00:20:50 -0700

    smart counter check uniqye

--- 06_weather_api/Master/local.js
+++ 06_weather_api/Master/local.js
@@ -16,40 +16,53 @@ function init_history () {
 }
 
 function update_history ( city ) {
-  $( "#history_counter" ).text( +$( "#history_counter" ).text() + 1 );
-  
+  city = city.trim();
   const count = +$( "#history_counter" ).text();
+  $( "#history_counter" ).text( count+ 1 );
 
   const hist = JSON.parse( window.localStorage.getItem( "history" )
   ) || {};
-  console.log('hist :>> ', hist);
-  hist[count] = city;
-  // console.log( 'exi :>> ', hist );
-  window.localStorage.setItem( "history", JSON.stringify( hist ) );
   
+  // console.log( 'hist :>> ', Object.keys( hist ).map( d => hist[ d ] ) );
+  const set = new Set( Object.keys( hist ).map( d => hist[ d ] ) );
+  console.log('set :>> ', set);
+  if (set.has(city)) {
+   console.log('city in recents'); 
+  } else {
+    hist[ count ] = city;
+    window.localStorage.setItem( "history", JSON.stringify( hist ) );
+    console.log('hist :>> ', hist);
+    append_button( city );
+  }
+  
+  
+}  
+
+function append_button ( city ) {
   let hist_btn = $( '<button>' );
-  
-  $( "#history_box" ).append(hist_btn);
-  
+
+  $( "#history_box" ).append( hist_btn );
+
   hist_btn.attr( "type", "button" )
-  .attr("data-name", city)
-  .attr(
-    "class",
-    "btn btn-outline-secondary btn-sm rounded-xl locally-stored mx-2"
+    .attr( "data-name", city )
+    .attr(
+      "class",
+      "btn btn-outline-secondary btn-sm rounded-xl locally-stored mx-2"
     );
-    
-  hist_btn.text(city);
-    
-  $.each($('.locally-stored'), function (i, v) { 
-  // console.log('i :>> ', i);  
+
+  hist_btn.text( city );
+
+  $.each( $( '.locally-stored' ), function ( i, v ) {
+    // console.log('i :>> ', i);  
     $( this ).on( "click", function () {
       const clicked = $( this ).text().trim();;
-      console.log('clicked :>> ', clicked);
-  
+      console.log( 'clicked :>> ', clicked );
+
       $( '#search-value' ).val( clicked );
-      forecast_five_days(clicked);
-      todays_weather(clicked);
-      update_slider(8, clicked); 
-    });
-  });
-}  
+      forecast_five_days( clicked );
+      todays_weather( clicked );
+      update_slider( 8, clicked );
+    } );
+  } );
+}
+
