00678b57a7018dc83fc26f000beb5c815338b046
attila5287 <atiturkoz@hotmail.com>
2021-02-13 02:58:13 -0700

    local works logically

--- 06_weather_api/Master/local.js
+++ 06_weather_api/Master/local.js
@@ -1,4 +1,9 @@
-
+// init_history(); 
+def_history();
+function init_history () {
+  window.localStorage.setItem("history", JSON.stringify({}));
+}
+  
 function reset_button () {
   console.log( 'test reset' );
   
@@ -7,28 +12,59 @@ function reset_button () {
   $( '#history_box' ).empty();
 }
 $( '#reset_button' ).on( "click", reset_button );
-function init_history () {
-  window.localStorage
-    .setItem( "history", JSON.stringify({}) );
+
+
+function def_history () {
     
   const def_cities = ["Pueblo", "Alamosa", "Durango", "Denver"];
   def_cities.forEach( ( c ) => update_history( c ) );
 }
-$("#defaults_button").on("click", init_history);
-
-
+$("#def_btn").on("click", def_history);
 function update_history ( city ) {
+  
+function append_button(city) {
+	let hist_btn = $("<button>");
+
+	$("#history_box").append(hist_btn);
+
+	hist_btn
+		.attr("type", "button")
+		.attr("data-name", city)
+		.attr(
+			"class",
+			"btn btn-outline-secondary btn-sm rounded-xl locally-stored mx-2"
+		);
+
+	hist_btn.text(city);
+
+	$.each($(".locally-stored"), function (i, v) {
+		// console.log('i :>> ', i);
+		$(this).on("click", function () {
+			const clicked = $(this).text().trim();
+			console.log("clicked :>> ", clicked);
+
+			$("#search-value").val(clicked);
+			forecast_five_days(clicked);
+			todays_weather(clicked);
+			update_slider(8, clicked);
+		});
+	});
+}
+
   city = city.trim();
 
   const hist = JSON.parse( window.localStorage.getItem( "history" )
-  ) || {};
+  );
 
-  // console.log( 'loc stored cities :>> ', Object.keys( hist ).map( d => hist[ d ] ) );
-  const set = new Set( Object.keys( hist ).map( d => hist[ d ] ) );
-  // console.log('set :>> ', set);
+  console.log( 'loc stored cities :>> ', Object.keys( hist ).map( d => hist[ d ] ) );
+  const cities = Object.keys( hist ).map( d => hist[ d ] );
+  console.log('hist :>> ', hist);
+  console.log('cities :>> ', cities);
+  const set = new Set( cities ) ;
+  console.log('set :>> ', set);
 
   const already_in_recents = set.has( city ); // bool
-  
+  console.log('alread :>> ', already_in_recents);
   if ( already_in_recents) {
   console.log(`${city} in recents`); 
   } else {// add button to hist box
@@ -42,32 +78,4 @@ function update_history ( city ) {
   
   
 }  
-
-function append_button ( city ) {
-  let hist_btn = $( '<button>' );
-
-  $( "#history_box" ).append( hist_btn );
-
-  hist_btn.attr( "type", "button" )
-    .attr( "data-name", city )
-    .attr(
-      "class",
-      "btn btn-outline-secondary btn-sm rounded-xl locally-stored mx-2"
-    );
-
-  hist_btn.text( city );
-
-  $.each( $( '.locally-stored' ), function ( i, v ) {
-    // console.log('i :>> ', i);  
-    $( this ).on( "click", function () {
-      const clicked = $( this ).text().trim();;
-      console.log( 'clicked :>> ', clicked );
-
-      $( '#search-value' ).val( clicked );
-      forecast_five_days( clicked );
-      todays_weather( clicked );
-      update_slider( 8, clicked );
-    } );
-  } );
-}
 
