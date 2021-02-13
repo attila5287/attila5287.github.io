39f89eb4dbfca2695d5c26131d5b2b04fcf858fd
attila5287 <atiturkoz@hotmail.com>
2021-02-13 02:10:30 -0700

    quick coverup for removing semi-static time on date

--- 06_weather_api/Master/momento.js
+++ 06_weather_api/Master/momento.js
@@ -1,20 +1,44 @@
 const current_hour = moment().hour();
 const s = $("#slider_hour");    
-const slider = $( "#slider" ) ;    
-const sval = slider.val();    
 update_slider_hour(current_hour + 24);
-slider.on("input", () =>{ 
+update_slider_date(current_hour + 24);
+
+const slider = $( "#slider" ) ;    
+slider.on( "input", () => { 
   const changed = +slider.val();
   // console.log('changed :>> ', changed);
-  
+
   update_slider_hour( changed );
-  
+  update_slider_date( changed );
 });
 
 function update_slider_hour ( val ) {
   if (+current_hour + ((val * 3) % 24) > 12) {
-		s.text(current_hour + ((val * 3) % 12) + " PM");
-	} else if (+current_hour + ((val * 3) % 24) < 12) {
-		s.text(current_hour + ((val * 3) % 12) + " AM");
-	}
+    s.text(current_hour + ((val * 3) % 12) + " PM");
+  } else if (+current_hour + ((val * 3) % 24) < 12) {
+    s.text(current_hour + ((val * 3) % 12) + " AM");
+  }
 }
+
+function update_slider_date(val) {
+	const hours_from_now = val * 3;
+  const day = Math.floor( hours_from_now / 24 );
+  
+  if (day<1) {
+    const date = moment().format('LL');
+    $("#slider_date").text(date);
+    console.log("day if :>> ", day);
+    console.log("date else :>> ", date);
+    
+  } else {
+    const date = moment().add(day, "days").calendar();
+    const on_print = date.slice(0, date.length - 11);
+    $( "#slider_date" ).text( on_print );
+
+
+  }
+
+}
+
+
+
