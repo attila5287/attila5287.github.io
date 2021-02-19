3620d6d37b480b220de1579500a3f682d700c450
attila5287 <atiturkoz@hotmail.com>
2021-02-19 04:08:27 -0700

    five day forecast toggle btn switch baCK

--- 06_weather_api/Master/fore_toggle.js
+++ 06_weather_api/Master/fore_toggle.js
@@ -3,6 +3,8 @@
 $("#fore_toggle").on(
   "click", () => {
 
+    $(".fore_div_f ").addClass('d-none');
+    $(".fore_div_c ").removeClass('d-none');
     $("#fore_toggle_mode").text('C' );
     $( "#fore_toggle" ).empty();
     const new_icon = $('<i>').attr('class', 'fas fa-toggle-on'); 
