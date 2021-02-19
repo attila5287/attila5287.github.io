cfd1588adc11f4a0cf949a1b8b44c6ad92667ad7
attila5287 <atiturkoz@hotmail.com>
2021-02-19 04:18:18 -0700

    forecast toggle works from F to C only

--- 06_weather_api/Master/fore_toggle.js
+++ 06_weather_api/Master/fore_toggle.js
@@ -2,12 +2,21 @@
 
 $("#fore_toggle").on(
   "click", () => {
-
-    $(".fore_div_f ").addClass('d-none');
-    $(".fore_div_c ").removeClass('d-none');
-    $("#fore_toggle_mode").text('C' );
-    $( "#fore_toggle" ).empty();
-    const new_icon = $('<i>').attr('class', 'fas fa-toggle-on'); 
-    $( "#fore_toggle" ).append(new_icon);
+    if ($("#fore_toggle").attr("data-tempunit") == "f") {
+			$(".fore_div_f ").addClass("d-none");
+			$(".fore_div_c ").removeClass("d-none");
+			$("#fore_toggle_mode").text("C");
+			$("#fore_toggle").empty();
+			const new_icon = $("<i>").attr("class", "fas fa-toggle-on");
+			$("#fore_toggle").append(new_icon);
+		}
+    if ($("#fore_toggle").attr("data-tempunit") == "c") {
+			$(".fore_div_c ").addClass("d-none");
+			$(".fore_div_f ").removeClass("d-none");
+			$("#fore_toggle_mode").text("F");
+			$("#fore_toggle").empty();
+			const new_icon = $("<i>").attr("class", "fas fa-toggle-on");
+			$("#fore_toggle").append(new_icon);
+		}
   }
 );
