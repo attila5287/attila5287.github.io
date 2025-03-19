ee876f12373a5f6565af730e856c9c91b68b19e8
Turkoz <turkoza@dpa.state.co.us>
2025-03-19 01:16:41 -0600

    del works with generate lines

--- mapbox3d/script.js
+++ mapbox3d/script.js
@@ -1,30 +1,24 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw';
 const map = new mapboxgl.Map({
     container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/standard',
     zoom: 18.75,
-    // center: [-104.985302, 39.740326], // St Services Bldg
-    // center: [-104.98863682037847, 39.74349648343346], // rep plaza
     center: [-104.98887493053121, 39.73899257929499], // civic
     pitch: 65,
     antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased
 });
+
+// #region MAP-ADDCONTROL(mapbox-gl-DRAW)
 const draw = new MapboxDraw({
     displayControlsDefault: true,
-    // Select which mapbox-gl-draw control buttons to add to the map.
-    // controls: {
-    //     polygon: true,
-    //     trash: true
-    // },
-    // Set mapbox-gl-draw to draw by default.
-    // The user does not have to click the polygon control button first.
     // defaultMode: 'draw_polygon'
 });
 map.addControl(draw);
 map.on('draw.create', updateArea);
 map.on('draw.delete', updateArea);
 map.on('draw.update', updateArea);
+// #endregion
+// #region AUTO-GENERATE LINES
 
 function autoGenerateLine(poly) {
     let buffered, simplified, smoother, featureColl;
@@ -35,7 +29,11 @@ function autoGenerateLine(poly) {
     let coords = [];
     let result = [];
     for (let index = 0; index < 5; index++) {
-        coords.push(smoother.features[0].geometry.coordinates.flatMap(d => d) )
+        if (smoother.features.length>0) {
+            coords.push(smoother.features[0].geometry.coordinates.flatMap(d => d) )
+        } else {
+            console.log('generate lines: no feats in geojson file')
+    }
     }
     console.log(coords.flatMap(d => d))
     result = coords.flatMap(d => d)
@@ -54,72 +52,26 @@ function autoGenerateLine(poly) {
     return JSON;
 }
 
-
-// #region AUTO-GENERATING -  GREEN EXTRUSION
-function autoGenerate(poly) {
-    let buffered, simplified, smoother, featureColl;
-    buffered = turf.buffer(poly, 0.006, { units: "kilometers" })
-    simplified = turf.simplify(buffered, { tolerance: 0.00001, highQuality: true });
-    smoother = turf.polygonSmooth(simplified, { iterations: 3 })
-    let feats = [];
-    let temp ;
-    const colors = [
-        'green',
-        'cyan',
-        'pink',
-        'purple',
-        'yellow',
-    ];
-
-    for (let index = 0; index < 5; index++) {
-        temp = {};
-        temp = JSON.parse(JSON.stringify(smoother)); // Create a deep copy of smoother
-        temp.features[0].properties['base'] =index*2;
-        temp.features[0].properties['height'] = (index *2) + 0.5;
-        temp.features[0].properties['color'] = colors[index];
-        // temp.features[0].properties['opacity'] = index*0.2;
-        temp.features[0]['id'] = 'green' + index
-        feats.push(temp.features[0]);
-    };
-    // console.log(feats)
-    featureColl = {
-        "features": feats,
-        "type": "FeatureCollection"
-    };
-    // console.log(featureColl)
-    return featureColl;
-    // console.log('smoother')
-    // console.log(smoother)
-    // return smoother;
-}
 // #endregion
-
-// #region USER-UPDATE-AREA -BLUE EXTRUSION
+// #region USER-DRAWn-POLY-*** updateArea()-*** -BLUE
 function updateArea(e) {
     const polygon = draw.getAll();
     const answer = document.getElementById('calculated-area');
     map.getSource('user-drawn-polygon').setData(polygon)
-    // map.getSource('auto-generated-polygon').setData(autoGenerate(polygon))
     map.getSource('auto-generated-line').setData(autoGenerateLine(polygon))
 
     if (polygon.features.length > 0) {
         const area = turf.area(polygon);
         const length = turf.length(polygon, { units: "meters" });
-        // Restrict the area to 2 decimal points.
         const roundedArea = Math.round(area * 100) / 100;
         const roundedLength = Math.round(length * 100) / 100;
-        answer.innerHTML = `
-          <strong>
-            ${roundedLength} meters <br>
-            ${roundedArea} sq-meters  /
-          </strong>
-        `;
+        answer.innerHTML = `${roundedLength} mt | ${roundedArea} sq-meters`;
 
         map.getSource('user-drawn-polygon').setData(polygon)
-        // map.getSource('auto-generated-polygon').setData(autoGenerate(polygon))
         map.getSource('auto-generated-line').setData(autoGenerateLine(polygon))
 
     } else {
+        
         answer.innerHTML = '';
         if (e.type !== 'draw.delete')
             alert('Click the map to draw a polygon.');
@@ -127,7 +79,7 @@ function updateArea(e) {
 }
 // #endregion
 
-// #region EVENT HANDL3RS in Chkboxes
+// #region HANDL3RS id's in Chkboxes map[handl3r] 
 const handlers = [
     "scrollZoom",
     "boxZoom",
@@ -137,14 +89,15 @@ const handlers = [
     "doubleClickZoom",
     "touchZoomRotate",
 ];
-
+// #endregion
 // #region CAM-CONTROLS disable-enable EACH
 let $camControls = document.querySelector("#navCamControls");
 $camControls
     .addEventListener("change", (e) => {
         const handler = e.target.id;
-        console.log(handler)
-        console.log(e.target.checked)
+        // console.log('handler')
+        // console.log(handler)
+        // console.log(e.target.checked)
         if (e.target.checked) {
             map[handler].enable();
         } else {
@@ -152,15 +105,14 @@ $camControls
         }
     });
 // #endregion
-
 // #region CAM-CONTROLS disable-enable ALL
 let $disableCamControls = document.querySelector("#disableCamControls");
 let isDisabled = false;
 $disableCamControls
     .addEventListener("change", (e) => {
-        console.log(`e.target.checked ${e.target.checked}`)
+        // console.log(`e.target.checked ${e.target.checked}`)
         document.querySelectorAll(".camCheckBox").forEach(h => {
-            console.log(`h: ${h.id}`)
+            // console.log(`h: ${h.id}`)
             if (h.id != "") {
                 if (e.target.checked) {
                     map[h.id].disable()
@@ -179,16 +131,16 @@ $disableCamControls
 let $showCam = document.querySelector("#showCamControls");
 $showCam
     .addEventListener("change", (e) => {
-        console.log(`ShowCamControls: ${e.target.checked}`)
+        // console.log(`ShowCamControls: ${e.target.checked}`)
         if (!e.target.checked) {
             $camControls.classList.toggle("animate__slideInDown");
             $camControls.style.display = "none"; // removes layout
-            console.log('camControls display: NONE')
+            // console.log('camControls display: NONE')
 
         } else {
             $camControls.classList.toggle("animate__slideInDown");
             $camControls.style.display = "block";
-            console.log('camControls display: BLOCK')
+            // console.log('camControls display: BLOCK')
         }
     });
 // #endregion
@@ -227,7 +179,7 @@ const modelTransform = {
     scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
 };
 // #endregion
-// #region  configuration of the custom layer for a 3D model per the CustomLayerInterface
+// #region Custom layer for 3D model: CustomLayerInterface
 const THREE = window.THREE;
 const customLayer = {
     id: 'threeJS',
@@ -353,7 +305,6 @@ const customLayer = {
     }
 };
 // #endregion
-
 // #region blank JSON to load when there is no polygon
 let blankJSON = {
     "type": "FeatureCollection",
@@ -361,11 +312,8 @@ let blankJSON = {
 };
 
 // #endregion
+
 map.on('style.load', () => {
-    map.addSource('auto-generated-polygon', {
-        'type': 'geojson',
-        'data': blankJSON,
-    });
     map.addSource('user-drawn-polygon', {
         'type': 'geojson',
         'data': blankJSON,
@@ -386,17 +334,6 @@ map.on('style.load', () => {
     });
     map.addLayer(customLayer);
 
-    // map.addLayer({
-    //     'id': 'generated-layer',
-    //     'type': 'fill-extrusion',
-    //     'source': 'auto-generated-polygon',
-    //     'paint': {
-    //         'fill-extrusion-color': ['get', 'color'],
-    //         'fill-extrusion-height': ['get', 'height'],
-    //         'fill-extrusion-base':   ['get', 'base'],
-    //         'fill-extrusion-opacity': 0.1
-    //     }
-    // });
     map.addLayer({
         'id': 'user-extrude-layer',
         'type': 'fill-extrusion',
