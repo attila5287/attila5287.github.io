626f068c79c4ece143fcc099c75626ecf617be18
Turkoz <turkoza@dpa.state.co.us>
2025-03-19 09:47:51 -0600

    dusk mode

--- mapbox3d/script.js
+++ mapbox3d/script.js
@@ -1,14 +1,95 @@
+let userExtrusionHeight = 16;
+let userStepCount = 4;
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw';
+let baseConfig = {
+    // Configuration for the Mapbox Standard style
+    basemap: {
+        lightPreset: 'day'
+    }
+};
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/standard',
+    config: baseConfig,
     zoom: 18.75,
     center: [-104.98887493053121, 39.73899257929499], // civic
     pitch: 65,
     antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased
 });
 
-// #region MAP-ADDCONTROL(mapbox-gl-DRAW)
+let $duskMode = document.querySelector("#duskModeCkbox");
+let isDusk = false;
+$duskMode.addEventListener('change', (e) => {
+    if (!e.target.checked) {
+        map.setConfigProperty('basemap', 'lightPreset', 'dusk');
+        isDusk = true;
+        $duskMode.setAttribute("checked", "checked")
+    } else { 
+        map.setConfigProperty('basemap', 'lightPreset', 'day');
+        isDusk = false;
+        $duskMode.removeAttribute("checked")
+    }
+});
+
+// #region auto generate lines around the blue extrusion
+function autoGenerateLine(poly) {
+    let buffered, simplified, smoother, featureColl;
+    buffered = turf.buffer(poly, 0.006, { units: "kilometers" })
+    simplified = turf.simplify(buffered, { tolerance: 0.00001, highQuality: true });
+    smoother = turf.polygonSmooth(simplified, { iterations: 3 })
+    // console.log(poly)
+    const featureCollection = {
+        'type': 'FeatureCollection',
+        'features': []
+    };
+    let elevMin, elevMax;
+    let calculatedStepHeight = userExtrusionHeight / userStepCount;
+    for (let index = 0; index < userStepCount; index++) {
+        if (smoother.features.length > 0) {
+            const feat = { // feat for Line layer
+                "type": "Feature",
+                "properties": {
+                    "id": 'feat-line-id-' + index,
+                    "level": 'level-index-' + index,
+                    "elevMin": elevMin,
+                    "elevMax": elevMax,
+                    "elevation": [
+                        index * calculatedStepHeight,
+                        (index * calculatedStepHeight) + calculatedStepHeight,
+                    ]
+                },
+                "geometry": {
+                    "coordinates": [],
+                    "type": "LineString"
+                }
+            };
+            let c = smoother.features[0].geometry.coordinates;
+            console.log(c)
+            if (index % 2 === 0) { // Check if the index is even
+                feat.geometry.coordinates.push(
+                    ...c.reverse().flatMap(d => d)
+                )
+            } else {
+                feat.geometry.coordinates.push(
+                    ...c.flatMap(d => d)
+                )
+                
+            }
+            console.log(index)
+            console.log(feat)
+            featureCollection.features.push(feat);
+
+
+        } else {
+            console.log('generate lines: no feats in geojson smoother polygon')
+        }
+
+    }
+    // console.log(featureCollection)
+    return featureCollection;
+}
+// #endregion
+// #region mapbox-gl-DRAW controls
 const draw = new MapboxDraw({
     displayControlsDefault: true,
     // defaultMode: 'draw_polygon'
@@ -18,79 +99,48 @@ map.on('draw.create', updateArea);
 map.on('draw.delete', updateArea);
 map.on('draw.update', updateArea);
 // #endregion
-// #region AUTO-GENERATE LINES
-
-function autoGenerateLine(poly) {
-    let buffered, simplified, smoother, featureColl;
-    buffered = turf.buffer(poly, 0.006, { units: "kilometers" })
-    simplified = turf.simplify(buffered, { tolerance: 0.00001, highQuality: true });
-    smoother = turf.polygonSmooth(simplified, { iterations: 3 })
-    // console.log(poly)
-    let coords = [];
-    let result = [];
-    for (let index = 0; index < 5; index++) {
-        if (smoother.features.length>0) {
-            coords.push(smoother.features[0].geometry.coordinates.flatMap(d => d) )
-        } else {
-            console.log('generate lines: no feats in geojson file')
-    }
-    }
-    console.log(coords.flatMap(d => d))
-    result = coords.flatMap(d => d)
-    console.log(result)
-    let JSON = {
-        "type": "Feature",
-        "properties": {
-            "elevation": [1, 15]
-        },
-        "geometry": {
-            "coordinates": result,
-            "type": "LineString"
-        }
-    }
-;
-    return JSON;
+// #region helper functions: round
+function roundByN(floatNum, numDecimals) {
+    const tenExp = 10^numDecimals;
+    const res = Math.round(floatNum * tenExp) / tenExp;
+    // console.log(`${res}: ${floatNum} rnd by ${numDecimals}`)
+    return res;
 }
-
 // #endregion
-// #region USER-DRAWn-POLY-*** updateArea()-*** -BLUE
+// #region ** updateArea()-*** -blue extrusion - main function
 function updateArea(e) {
     const polygon = draw.getAll();
     const answer = document.getElementById('calculated-area');
     map.getSource('user-drawn-polygon').setData(polygon)
-    map.getSource('auto-generated-line').setData(autoGenerateLine(polygon))
+    map.getSource('line-src').setData(autoGenerateLine(polygon))
 
     if (polygon.features.length > 0) {
         const area = turf.area(polygon);
         const length = turf.length(polygon, { units: "meters" });
-        const roundedArea = Math.round(area * 100) / 100;
-        const roundedLength = Math.round(length * 100) / 100;
-        answer.innerHTML = `${roundedLength} mt | ${roundedArea} sq-meters`;
+        answer.innerHTML = `${roundByN(length, 2)} mt <br>${roundByN(area, 2)} sq-mt`;
 
         map.getSource('user-drawn-polygon').setData(polygon)
-        map.getSource('auto-generated-line').setData(autoGenerateLine(polygon))
+        map.getSource('line-src').setData(autoGenerateLine(polygon))
 
     } else {
-        
         answer.innerHTML = '';
         if (e.type !== 'draw.delete')
             alert('Click the map to draw a polygon.');
     }
 }
 // #endregion
-
-// #region HANDL3RS id's in Chkboxes map[handl3r] 
+// #region map camera control events: map[handl3r]-dis4ble() 
 const handlers = [
     "scrollZoom",
     "boxZoom",
     "dragRotate",
     "dragPan",
     "keyboard",
-    "doubleClickZoom",
+    "doubleClickZoom", // can not be disabled per draw controls
     "touchZoomRotate",
 ];
 // #endregion
-// #region CAM-CONTROLS disable-enable EACH
+// #region disable-enable each map camera control
 let $camControls = document.querySelector("#navCamControls");
 $camControls
     .addEventListener("change", (e) => {
@@ -105,9 +155,10 @@ $camControls
         }
     });
 // #endregion
+
 // #region CAM-CONTROLS disable-enable ALL
 let $disableCamControls = document.querySelector("#disableCamControls");
-let isDisabled = false;
+let isDisabledAll = false;
 $disableCamControls
     .addEventListener("change", (e) => {
         // console.log(`e.target.checked ${e.target.checked}`)
@@ -117,11 +168,11 @@ $disableCamControls
                 if (e.target.checked) {
                     map[h.id].disable()
                     document.getElementById(h.id).removeAttribute("checked")
-                    isDisabled = true
+                    isDisabledAll = true
                 } else {
                     document.getElementById(h.id).setAttribute("checked", "checked")
                     map[h.id].enable()
-                    isDisabled = false
+                    isDisabledAll = false
                 }
             }
         });
@@ -151,9 +202,9 @@ map.on('mousemove', (e) => {
     $info.setAttribute('positionY', e.point.y)
     $info.setAttribute('lng', e.lngLat.lng)
     $info.setAttribute('lat', e.lngLat.lat)
-
+    // TODO lets make this a table with rounded numbers so it wouldn't take so much space
     // console.log(e.lngLat.lng + " " + e.lngLat.lat)
-    $info.innerHTML = `${JSON.stringify(e.point)}<br />${JSON.stringify(e.lngLat.wrap())}`;
+    $info.innerHTML = `${JSON.stringify(e.point)}<br/> ${JSON.stringify(e.lngLat.wrap())}`;
 });
 // #endregion
 // #region MODEL TRANSFORMATION
@@ -179,7 +230,7 @@ const modelTransform = {
     scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
 };
 // #endregion
-// #region Custom layer for 3D model: CustomLayerInterface
+// #region Custom layer for 3D model: from documentation
 const THREE = window.THREE;
 const customLayer = {
     id: 'threeJS',
@@ -219,50 +270,10 @@ const customLayer = {
             antialias: true
         });
 
-        // #region DOUBLE CLICK EVENT = not used
-        let posX, posY;
-        let lngPoint, latPoint, coordsPoint;
-        function merCoords(ln, lt) {
-            let tempJSON = {
-                lng: ln,
-                lat: lt
-            };
-            console.log('merCoords: tempJSON')
-            console.log(tempJSON)
-            let res = mapboxgl.MercatorCoordinate.fromLngLat(tempJSON);
-            console.log('merCoords: res merc')
-            console.log(res)
-            return res;
-        }
-        function dblClkHandlerMoveScene(e) {
-            const rect = map.getCanvas().getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
-            const lngLat = map.unproject([x, y]);
-            const mercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(lngLat);
-
-            console.log('New scene position1:', this.scene.position);
-            this.scene.position.set(mercatorCoordinate.x, mercatorCoordinate.y, this.scene.position.z);
-            console.log('New scene position2:', this.scene.position);
-            //     posX = $info.getAttribute("positionX")
-            //     posY = $info.getAttribute("positionY")
-            //     lngPoint = $info.getAttribute("lng")
-            //     latPoint = $info.getAttribute("lat")
-            //     coordsPoint = merCoords(lngPoint, latPoint)
-            //     const lngLat = map.unproject([e.clientX, e.clientY]);
-            //     const mercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(lngLat);
-            //     this.scene.position.set(mercatorCoordinate.x, mercatorCoordinate.y, this.scene.position.z);
-            //     this.scene.position.set(10,0,0)
-        };
-
-
-
-
-
-        // #endregion
-
         this.renderer.autoClear = false;
     },
+
+    // #region RENDER
     render: function (gl, matrix) {
         const rotationX = new THREE.Matrix4().makeRotationAxis(
             new THREE.Vector3(1, 0, 0),
@@ -303,9 +314,11 @@ const customLayer = {
         this.map.triggerRepaint();
         // console.log(this)
     }
+    // #endregion
+
 };
 // #endregion
-// #region blank JSON to load when there is no polygon
+// #region blank geoJSON data to load when there is no polygon
 let blankJSON = {
     "type": "FeatureCollection",
     "features": []
@@ -313,27 +326,65 @@ let blankJSON = {
 
 // #endregion
 
+const lineGeoJSON = {
+    type: "Feature",
+    properties: {
+        elevation: [],
+    },
+    geometry: {
+        coordinates: [],
+        type: "LineString",
+    },
+};
+
 map.on('style.load', () => {
+    map.addLayer(customLayer);
     map.addSource('user-drawn-polygon', {
         'type': 'geojson',
         'data': blankJSON,
     });
-    map.addSource("auto-generated-line", {
+    map.addSource("line-src", {
         type: "geojson",
         lineMetrics: true,
-        data: {
-            type: "Feature",
-            properties: {
-                elevation: [1, 15],
-            },
-            geometry: {
-                coordinates: [],
-                type: "LineString",
-            },
-        },
+        data: lineGeoJSON,
     });
-    map.addLayer(customLayer);
-
+    const paintLine = {
+        "line-emissive-strength": 1.0,
+        "line-blur": 2,
+        "line-width": 5,
+        "line-color": "green",
+        "line-gradient": [
+            "interpolate",
+            ["linear"],
+            ["line-progress"],
+            0.0,
+            "cyan",
+            0.5,
+            "lime",
+            0.7,
+            "yellow",
+            1,
+            "cyan",
+        ],
+        // 'line-trim-color':'red',
+        // 'line-trim-offset' : [0.00, 0.002],
+        // 'line-gap-width': 1,
+    };
+    let layoutLine = {
+        "line-z-offset": [
+            "at",
+            [
+                "*",
+                ["line-progress"],
+                ["-", ["length", ["get", "elevation"]], 1],
+            ],
+            ["get", "elevation"],
+        ],
+        "line-elevation-reference": "sea",
+        "line-cap": "round",
+    };
+    
+    
     map.addLayer({
         'id': 'user-extrude-layer',
         'type': 'fill-extrusion',
@@ -345,43 +396,23 @@ map.on('style.load', () => {
             'fill-extrusion-opacity': 0.3
         }
     });
+
+    layoutLine['line-cross-slope']= 0
     map.addLayer({
-        id: "elevated-line",
+        id: "elevated-line1",
         type: "line",
-        source: "auto-generated-line",
-        layout: {
-            "line-z-offset": [
-                "at",
-                [
-                    "*",
-                    ["line-progress"],
-                    ["-", ["length", ["get", "elevation"]], 1],
-                ],
-                ["get", "elevation"],
-            ],
-            "line-elevation-reference": "sea",
-            "line-cap": "round",
-            "line-join": "round",
-        },
-        paint: {
-            "line-emissive-strength": 2.0,
-            "line-width": 4,
-            "line-color": "green",
-            "line-gradient": [
-                "interpolate",
-                ["linear"],
-                ["line-progress"],
-                0.0,
-                "cyan",
-                0.5,
-                "lime",
-                0.7,
-                "yellow",
-                1,
-                "pink",
-            ],
-            // 'line-trim-offset' : [0.2, 0.8],
-            'line-gap-width' :3,
-        },
+        source: "line-src",
+        layout: 
+        layoutLine,
+        paint: paintLine,
+    });
+    
+    layoutLine['line-cross-slope'] = 1
+    map.addLayer({
+        id: "elevated-line2",
+        type: "line",
+        source: "line-src",
+        layout:layoutLine,
+        paint: paintLine,
     });
 });
